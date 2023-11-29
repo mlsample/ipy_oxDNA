1bd3cee67bbaf295ec3028c08ff2e87daa9c2ac9
mlsample <matsample1@gmail.com>
2023-11-29 14:56:17 -0800

    finally, make my package pip installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipy_oxdna',
+    version='0.21',
+    packages=find_packages(),
+    description='Enables the use of oxDNA in Jupyter notebooks.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mlsample/ipy_oxDNA',
+    author='Matthew Sample',
+    author_email='matsample1@gmail.com',
+    license='MIT',
+)
+    # install_requires=[
+    #     # List of dependencies
+    # ],
+    # classifiers=[
+    #     # Package classifiers
+    # ],
+#)
+
